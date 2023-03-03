2d09b07bb947cac03e1a9ac36d09ed33210077ed
Muthuraj Mohan kumar <mrohini116@gmail.com>
2023-03-03 11:14:21 +0000

    home design cahnge

--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -1,3 +1,12 @@
-const Home: React.FC = () => <>Home!</>;
+const Home: React.FC = () => (
+  <>
+    <h3>Welcome to the home of Justice Department of Fakelandia!</h3>
+    <br></br>
+    <p>
+      Here we can browse list of misdemeanours commited by our citizens or you
+      canconfess of your own misdemeanours
+    </p>
+  </>
+);
 
 export default Home;
